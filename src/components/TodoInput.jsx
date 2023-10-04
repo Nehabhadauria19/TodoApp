8690092f74733794fe918809c4ca96954a0cce59
Nehabhadauria19 <146559822+Nehabhadauria19@users.noreply.github.com>
2023-10-04 17:40:26 +0530

    Update TodoInput.jsx

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,58 +1,79 @@
-import React, { useState } from 'react'
-import "../App.css"
-<link
-  href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
-  rel="stylesheet"
-/>
-
+import React, { useState } from 'react';
+import "../App.css";
 
 const TodoInput = () => {
-    const[task,setTask]=useState("") // for inp tag
-    const[taskListed,setTaskListed]=useState([]) // for button
-    const[edit,setEdit]=useState(false)
-    const[id,setId]=useState("")
+  const [newTask, setNewTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [taskIdToEdit, setTaskIdToEdit] = useState(null);
 
-    let addButton=(e)=>{
-        e.preventDefault()
-        setTaskListed([...taskListed,task]) //store previous elem along with current elem 
-        setTask("")
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    if (newTask.trim() === "") {
+      return; // Don't add empty tasks
     }
-    let DeleteClick=(id)=>{
-         setTaskListed([...taskListed].filter((ele,eleId)=>eleId!==id))
-       
+    if (editMode) {
+      // Edit existing task
+      const updatedTaskList = [...taskList];
+      updatedTaskList[taskIdToEdit] = newTask;
+      setTaskList(updatedTaskList);
+      setEditMode(false);
+    } else {
+      // Add a new task
+      setTaskList([...taskList, newTask]);
     }
-    let EditClick=(id)=>{
-      const items= taskListed.find((ele,eleId)=>eleId===id)
-      setTask(items)
-      setEdit(!edit)
-      setId(id)
-    }
-    let EditButton=()=>{
-      taskListed.splice(id,1,task)
-      setEdit(!edit)
-      setTask("")
-    }
-    
-     
+    setNewTask("");
+    setTaskIdToEdit(null);
+  };
+
+  const handleDeleteTask = (id) => {
+    const updatedTaskList = taskList.filter((_, index) => index !== id);
+    setTaskList(updatedTaskList);
+  };
+
+  const handleEditTask = (id) => {
+    setNewTask(taskList[id]);
+    setEditMode(true);
+    setTaskIdToEdit(id);
+  };
+
   return (
-    <form className='main-container' onSubmit={addButton}> 
-    <h3 className='head'>ToDoiee App</h3>
-    {edit ? <input className='inputs' type="text" placeholder='Edit your task' onChange={(e)=>setTask(e.target.value)} value={task}/>
-    :<input className='inputs' type="text" placeholder='Add your task' onChange={(e)=>setTask(e.target.value)} value={task}/>}
-    {edit ? <button className='upd' onClick={EditButton}>Update</button>
-   : <button className='btn'>ADD</button>}
-    <br />
-    <ol>
-     {taskListed.map((item,index)=>(
-      <li className='list'>{item} <span className='edit'onClick={()=>EditClick(index)}>Edit</span> <span className='delete' onClick={()=>DeleteClick(index) }>Delete</span></li>
-      
-     ))}
-     </ol>
-     
-      
-     
+    <form className='main-container' onSubmit={handleAddTask}>
+      <h3 className='head'>ToDoiee App</h3>
+      {editMode ? (
+        <input
+          className='inputs'
+          type="text"
+          placeholder='Edit your task'
+          onChange={(e) => setNewTask(e.target.value)}
+          value={newTask}
+        />
+      ) : (
+        <input
+          className='inputs'
+          type="text"
+          placeholder='Add your task'
+          onChange={(e) => setNewTask(e.target.value)}
+          value={newTask}
+        />
+      )}
+      {editMode ? (
+        <button className='upd' onClick={handleAddTask}>Update</button>
+      ) : (
+        <button className='btn'>ADD</button>
+      )}
+      <br />
+      <ol>
+        {taskList.map((task, index) => (
+          <li className='list' key={index}>
+            {task}
+            <span className='edit' onClick={() => handleEditTask(index)}>Edit</span>
+            <span className='delete' onClick={() => handleDeleteTask(index)}>Delete</span>
+          </li>
+        ))}
+      </ol>
     </form>
-  )
-}
+  );
+};
 
-export default TodoInput
+export default TodoInput;
